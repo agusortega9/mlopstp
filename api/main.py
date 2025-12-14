2ee8738471aa786dc0736e8109eca57040d255f6
agusortega <agustinignacioortega@gmail.com>
2025-12-14 14:18:10 -0300

    tidy up code

--- api/main.py
+++ api/main.py
@@ -9,10 +9,6 @@ import os
 app = FastAPI(title="AdTech Recommendations API")
 
 # --- DATABASE CONFIG ---
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
 DB_PORT = "5432"
 
 def get_db_connection():
